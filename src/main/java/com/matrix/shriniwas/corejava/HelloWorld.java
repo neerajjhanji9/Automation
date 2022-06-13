0cb1050e3cba3ada2cec0e7c41060f660ece9ba4
shriniwasalle <shriniwasalle4@gmail.com>
2022-06-13 22:13:49 +0530

    Initial commit

---
+++ src/main/java/com/matrix/shriniwas/corejava/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.matrix.shriniwas.corejava;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to the Automation world!");
+    }
+}
