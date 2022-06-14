8a7f28d719b792c0ab281ab892d5e9a7cbe0262a
kishormane2023 <kishormane2023@gmail.com>
2022-06-14 12:36:51 +0530

    Initial Program

---
+++ src/main/java/com/matrix/kishor/corejava/FirstAdd.java
@@ -0,0 +1,13 @@
+package com.matrix.kishor.corejava;
+
+public class FirstAdd {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        int c;
+        c=a+b;
+        System.out.println("C="+c);
+
+    }
+}
