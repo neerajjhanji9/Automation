1a7cc8b9dd1f71f838e69a6fccf275b7c5efb8ad
kishormane2023 <kishormane2023@gmail.com>
2022-06-14 11:58:33 +0530

    Initial Program

--- src/main/java/com/matrix/shriniwas/corejava/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.matrix.shriniwas.corejava;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, Welcome to the Automation world!");
-    }
-}
