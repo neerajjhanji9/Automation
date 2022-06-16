65c06faeabf543ce9847bcf572713bd31e0bbe57
Akhilesh Dandi <akhileshdandi@gmail.com>
2022-06-16 20:08:51 +0530

    hello program

--- src/main/java/com/matrix/akhilesh/demoClass.java
+++ src/main/java/com/matrix/akhilesh/demoClass.java
@@ -3,6 +3,6 @@ package com.matrix.akhilesh;
 public class demoClass {
 
     public static void main(String[] args) {
-        System.out.println("1st program of core java");
+        System.out.println("Welcome to selenium");
     }
 }
