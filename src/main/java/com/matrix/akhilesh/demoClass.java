7d7cbf1d6452f0823e4d7fe36ee087e348e6795a
Akhilesh Dandi <akhileshdandi@gmail.com>
2022-06-16 19:59:45 +0530

    hello program

--- src/main/java/com/matrix/akhilesh/demoClass.java
+++ src/main/java/com/matrix/akhilesh/demoClass.java
@@ -3,6 +3,6 @@ package com.matrix.akhilesh;
 public class demoClass {
 
     public static void main(String[] args) {
-        System.out.println("Welcome To");
+        System.out.println("Welcome To India");
     }
 }
