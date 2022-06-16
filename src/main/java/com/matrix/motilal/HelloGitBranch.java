9dd46420f62785ca8f699fbfb21620514be7291a
shriniwasalle <shriniwasalle4@gmail.com>
2022-06-16 21:45:29 +0530

    Added new java file

---
+++ src/main/java/com/matrix/motilal/HelloGitBranch.java
@@ -0,0 +1,8 @@
+package com.matrix.motilal;
+
+public class HelloGitBranch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to Git branching concept!");
+    }
+}
