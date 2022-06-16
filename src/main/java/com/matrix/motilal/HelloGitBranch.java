5ccf4c9d755ddad8863243d24839f784c79eb94f
shriniwasalle <shriniwasalle4@gmail.com>
2022-06-16 22:10:45 +0530

    Added new sout statement

--- src/main/java/com/matrix/motilal/HelloGitBranch.java
+++ src/main/java/com/matrix/motilal/HelloGitBranch.java
@@ -4,5 +4,6 @@ public class HelloGitBranch {
 
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to Git branching concept!");
+        System.out.println("Hello, New Statement!");
     }
 }
