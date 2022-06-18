26fc09ad0145ee00ff788a92f5015f54ede695cb
Dhanashri Bhosale <dhanshree.bhosale16@gmail.com>
2022-06-18 14:45:16 +0530

    statement added in file

--- src/main/java/com/matrix/dhanashri/GitAdd.java
+++ src/main/java/com/matrix/dhanashri/GitAdd.java
@@ -1,4 +1,9 @@
 package com.matrix.dhanashri;
 
 public class GitAdd {
+
+    public static void main(String[] args) {
+
+        System.out.println("Added file successfully by using Git commands");
+    }
 }
