11e8b7d0676f7e9984e2eae8dbaa93a66b23a983
Vipin Yadav <vipin.david72@gmail.com>
2022-06-18 14:20:11 +0530

    Java Addition Program

---
+++ src/main/java/com/matrix/Vipin/JavaAddition.java
@@ -0,0 +1,14 @@
+package com.matrix.Vipin;
+
+public class JavaAddition {
+    public static void main(String[] args) {
+        System.out.printf("Enter two number");
+        int first = 10;
+        int second = 20;
+        // add two numbers
+        int sum = first + second;
+        System.out.printf("Sum of these numbers:" + sum);
+
+
+    }
+}
