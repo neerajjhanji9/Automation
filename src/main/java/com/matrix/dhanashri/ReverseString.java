29d17606a236f2f24cce464073db47f350ababf4
Dhanashri Bhosale <dhanshree.bhosale16@gmail.com>
2022-06-18 14:10:46 +0530

    Added Reverse String Program

---
+++ src/main/java/com/matrix/dhanashri/ReverseString.java
@@ -0,0 +1,23 @@
+package com.matrix.dhanashri;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str="Dhanashri";
+        String rvs=" ";
+        char ch;
+
+        System.out.println(str);
+        System.out.println(str.length());
+
+        for(int i=0; i<str.length(); i++) {
+
+            ch = str.charAt(i);
+            rvs = ch + rvs;
+
+        }
+        System.out.println("The reverse string is:"   +rvs);
+
+
+        }
+    }
+
