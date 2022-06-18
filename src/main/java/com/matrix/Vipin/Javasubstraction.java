016e1fc68982df29c6f7beab397a249fb210cc9c
Vipin Yadav <vipin.david72@gmail.com>
2022-06-18 15:09:11 +0530

    Java Addition Program

---
+++ src/main/java/com/matrix/Vipin/Javasubstraction.java
@@ -0,0 +1,9 @@
+package com.matrix.Vipin;
+
+public class Javasubstraction {
+    public static void main(String[] args) {
+        int firstNumber = 55;
+        int secondNumber = 5;
+        System.out.printf("firstNumber - secondNumber=" +(firstNumber-secondNumber));
+    }
+}
