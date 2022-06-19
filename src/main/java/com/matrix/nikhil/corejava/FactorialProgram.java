621ad34faef5b889b84921fbec37f8d7ba9210df
nikhilalle <allenikhilalle@gmail.com>
2022-06-20 00:08:20 +0530

    All Simple Programs New

---
+++ src/main/java/com/matrix/nikhil/corejava/FactorialProgram.java
@@ -0,0 +1,13 @@
+package com.matrix.nikhil.corejava;
+
+public class FactorialProgram {
+    public static void main(String[] args) {
+        int i,fact=1;
+        int no=5;
+        for (i=1;i<=no;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("fact of "+no+"  is "+fact);
+    }
+}
---
+++ src/main/java/com/matrix/nikhil/corejava/ForLoop2.java
@@ -0,0 +1,18 @@
+package com.matrix.nikhil.corejava;
+
+public class ForLoop2 {
+    {
+        for (int i=1;i<=5; i++)
+        {
+            for (int j=10;j<=15;j++){
+                System.out.println(i);
+                System.out.println("--------");
+            }
+
+    }
+        }
+
+    public static void main(String[] args) {
+        new ForLoop2();
+    }
+}
