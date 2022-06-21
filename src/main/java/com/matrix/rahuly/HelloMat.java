c76f72dc5eb778a487e68c63ff95aacfcef3f065
Rahulyelgetti <rahulyelgetti444@gmail.com>
2022-06-21 12:06:28 +0530

    Added new statement

--- src/main/java/com/matrix/rahuly/HelloMat.java
+++ src/main/java/com/matrix/rahuly/HelloMat.java
@@ -3,6 +3,6 @@ package com.matrix.rahuly;
 public class HelloMat {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to MAT-15 Rahul");
+        System.out.println("Practicing git commands");
     }
 }
