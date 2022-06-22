8ad723ae57f3a93dc3a2e3060ea5a1a22dd86879
shriniwasalle <shriniwasalle4@gmail.com>
2022-06-22 22:43:49 +0530

    Variables Concept

--- src/main/java/com/matrix/shriniwas/corejava/variables/LocalVariables.java
+++ src/main/java/com/matrix/shriniwas/corejava/variables/LocalVariables.java
@@ -2,23 +2,38 @@ package com.matrix.shriniwas.corejava.variables;
 
 public class LocalVariables {
 
-    public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
-    }
-
     // Method
     public void getData() {
         // Code, Logic
     }
 
     // Constructor
-    LocalVariables() {
+    public LocalVariables() {
         int b = 20;
+        String str = "Shriniwas";
+        int arr [] = {10, 20};
+        int age = 50;
+        System.out.println(b);
+        System.out.println(arr);
     }
 
     // Block
     {
-       int a = 50;
+        int a = 50;
+        System.out.println(a);
+    }
+
+    public void getDetails(int age) {
+        // Local variable
+        int data = 10;
+        System.out.println(data);
+        System.out.println(age);
+    }
+
+    public static void main(String[] args) {
+        // local variables
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
     }
 }
