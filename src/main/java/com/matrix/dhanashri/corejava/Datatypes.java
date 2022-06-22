c1df78a70601aa39a8ef05d76419fb401db1bd19
Dhanashri Bhosale <dhanshree.bhosale16@gmail.com>
2022-06-22 13:54:31 +0530

    Simple Datatype program added

---
+++ src/main/java/com/matrix/dhanashri/corejava/Datatypes.java
@@ -0,0 +1,19 @@
+package com.matrix.dhanashri.corejava;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Datatypes {
+    static int rollno = 5;
+    static String s = "Dhanashri";
+    static float fl = 1.9f;
+    static double db = 2.3f;
+    static boolean b = true;
+    {
+    System.out.println(rollno);
+    System.out.println("s");
+    System.out.println(fl);
+    System.out.println(db);
+    System.out.println(b);
+}
+
+}
