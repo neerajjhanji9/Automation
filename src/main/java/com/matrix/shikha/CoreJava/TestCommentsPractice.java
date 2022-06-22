b8db30c190d779c3aa48b16974c683c082b6b0bd
ShikhaUdaiPratapSingh <shikha.prasang@gmail>
2022-06-22 10:40:10 -0400

    Added new Test Comments Practice

---
+++ src/main/java/com/matrix/shikha/CoreJava/TestCommentsPractice.java
@@ -0,0 +1,22 @@
+package com.matrix.shikha.corejava;
+/**
+ * @author : Shikha
+ */
+
+public class TestCommentsPractice {
+
+    /**
+     * {@param Strinbg[]}
+     * This is a main method
+     */
+    public static void main(String[] args) {
+//        System.out.println("Hello, Shikha!");
+        System.out.println("How are you doing today?");
+       /* System.out.println("How are you doing today?");
+        System.out.println("How are you doing today?");
+        System.out.println("How are you doing today?");
+        System.out.println("How are you doing today?");
+        System.out.println("How are you doing today?"); */
+
+    }
+}
