286344c20fc4cedceeaa9075752696b9f39880f8
Gayathri-Arangi <gayathri.arangi@gmail.com>
2022-06-22 11:46:44 +0530

    Added new sout statement

--- src/main/java/com/matrix/gayathri/HelloGitBranch.java
+++ src/main/java/com/matrix/gayathri/HelloGitBranch.java
@@ -3,7 +3,8 @@ package com.matrix.gayathri;
 public class HelloGitBranch {
 
     public static void main(String[] args) {
-        System.out.println("Hello Welcome Git Branching Concept ");
+        System.out.println("Hello Welcome to Git Branching Concept ");
+        System.out.println("Added New sout statement");
     }
 
 }
