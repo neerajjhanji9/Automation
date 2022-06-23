ae067c5c29aea5f8adf951606cbcd83e411fd627
Akhilesh Dandi <akhileshdandi@gmail.com>
2022-06-23 15:55:12 +0530

    DataTypes Program

---
+++ src/main/java/com/matrix/akhilesh/datatypes/DataTypes.java
@@ -0,0 +1,31 @@
+package com.matrix.akhilesh.datatypes;
+
+public class DataTypes {
+
+    static int rollNumber;
+    static byte by;
+    static boolean b;
+    static char chr;
+    static float fl;
+    static double dl;
+    static String st;
+
+    public static void main(String[] args) {
+        int age = 40;
+        float f = 2.4f;
+        boolean a = false;
+        char c = 'A';
+        double d = 10.2;
+        String str = "Akhilesh";
+        System.out.println(age);
+        System.out.println(f);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(str);
+        System.out.println("-----");
+        System.out.println(rollNumber);
+        System.out.println(by);
+        System.out.println(b);
+    }
+}
