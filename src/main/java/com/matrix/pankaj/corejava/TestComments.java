d0f02006f80b280d84dc9a6d103e360ba4ddc2b5
HP <pankajkhobragade17@gmail.com>
2022-06-23 21:37:32 +0530

    Test comments

---
+++ src/main/java/com/matrix/pankaj/corejava/TestComments.java
@@ -0,0 +1,30 @@
+package com.matrix.pankaj.corejava;
+
+
+/*  @author : pankaj
+
+ */
+public class TestComments {
+
+    public static void main(String[]  args)  {
+         System.out.println("Hello Pankaj!");
+         System.out.println("Welcome to Java!");
+         System.out.println("Java practice");
+
+
+        /*System.out.println("Hello Pankaj!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Java practice");
+        System.out.println("Hello Pankaj!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Java practice"); */
+
+
+
+
+
+
+    }
+}
+
+
---
+++ src/main/java/com/matrix/pankaj/corejava/datatypes/TestDataTypes.java
@@ -0,0 +1,7 @@
+package com.matrix.pankaj.corejava.datatypes;
+
+public class TestDataTypes {
+    public static void main(String[] args) {
+
+    }
+}
