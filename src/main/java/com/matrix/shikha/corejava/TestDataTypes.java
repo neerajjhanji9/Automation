40527f5bffa98b1e784c8afce25c4d69ceb9b797
ShikhaUdaiPratapSingh <shikha.prasang@gmail>
2022-06-22 23:25:22 -0400

    Added new Test Data Types Practice

---
+++ src/main/java/com/matrix/shikha/corejava/TestDataTypes.java
@@ -0,0 +1,19 @@
+package com.matrix.shikha.corejava;
+
+public class TestDataTypes {
+    public static void main(String[] args) {
+        int age = 10;
+        boolean a = true;
+        char ch = 'S';
+        float f = 10.5f;
+        double d = 10.15;
+
+        System.out.println(age);
+        System.out.println(a);
+        System.out.println(ch);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println("\t"+ "My age is " + age +" " + a);
+
+    }
+}
