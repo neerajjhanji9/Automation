11e0a65349adf95c12aa2f1e69e1bd7219300e6c
Amita Mandal <amitasingam@gmail.com>
2022-06-23 15:58:49 +0530

    Added new sout statement

--- src/main/java/com/matrix/amita/corejava/HelloGitConcepts.java
+++ src/main/java/com/matrix/amita/corejava/HelloGitConcepts.java
@@ -3,6 +3,7 @@ package com.matrix.amita.corejava;
 public class HelloGitConcepts {
 
     public static void main(String[] args) {
-        System.out.println("Hello Welcome to Git branch concepts");
+        System.out.println("Hello, Welcome to Git branch concepts");
+        System.out.println("This is concept of git branch");
     }
 }
