070815f07176a1cd1123d191913e545412578737
Amita Mandal <amitasingam@gmail.com>
2022-06-23 16:07:31 +0530

    Added new sout statement for practice

--- src/main/java/com/matrix/amita/corejava/MasterBranchPractice.java
+++ src/main/java/com/matrix/amita/corejava/MasterBranchPractice.java
@@ -1,4 +1,9 @@
 package com.matrix.amita.corejava;
 
 public class MasterBranchPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Master Branch Concepts");
+        System.out.println("Hello Master Branch");
+    }
 }
