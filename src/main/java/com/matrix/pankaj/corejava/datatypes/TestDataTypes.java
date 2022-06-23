220f48a280115618fcf65950fa4ee95a0a982e1c
HP <pankajkhobragade17@gmail.com>
2022-06-23 22:14:07 +0530

    simple program

--- src/main/java/com/matrix/pankaj/corejava/datatypes/TestDataTypes.java
+++ src/main/java/com/matrix/pankaj/corejava/datatypes/TestDataTypes.java
@@ -1,7 +1,9 @@
 package com.matrix.pankaj.corejava.datatypes;
 
 public class TestDataTypes {
+
     public static void main(String[] args) {
+        System.out.println("This is java");
 
     }
 }
