efbbee393af95343961b3c5c1f42708b30a2c7db
Gayathri-Arangi <gayathri.arangi@gmail.com>
2022-06-24 10:01:19 +0530

    added LocalVariables

---
+++ src/main/java/com/matrix/gayathri/variables/LocalVariables.java
@@ -0,0 +1,36 @@
+package com.matrix.gayathri.variables;
+
+public class LocalVariables {
+
+    //Method
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+
+  // Constructor
+      public LocalVariables() {
+        int age =15;
+        char ch = 'G';
+        String s = "Gayathri";
+        System.out.println(ch);
+        }
+   // Block
+   {
+       int num = 90;
+       System.out.println(num);
+
+   }
+
+       public void getDetails(int age) {
+        int data = 16;
+       System.out.println(data);
+       System.out.println(age);
+   }
+
+
+    }
+
+
+
