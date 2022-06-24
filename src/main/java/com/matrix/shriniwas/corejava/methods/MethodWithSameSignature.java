52f578415e68da4bc2d5aff72b70b10de2ef4cfd
shriniwasalle <shriniwasalle4@gmail.com>
2022-06-24 22:46:56 +0530

    Methods Concept

---
+++ src/main/java/com/matrix/shriniwas/corejava/methods/MethodWithSameSignature.java
@@ -0,0 +1,16 @@
+package com.matrix.shriniwas.corejava.methods;
+
+public class MethodWithSameSignature {
+
+    public void m1(String str1) {
+        System.out.println(str1);
+    }
+
+    /* public void m1(String str2) {
+
+    } */
+
+    public static void main(String[] args) {
+
+    }
+}
