7e7263d6d377070a60918baf1adda9a8200538d2
Dhanashri Bhosale <dhanshree.bhosale16@gmail.com>
2022-06-25 15:04:28 +0530

    All Variable Concept files is added.

---
+++ src/main/java/com/matrix/dhanashri/corejava/InstanceVariablesNew.java
@@ -0,0 +1,15 @@
+package com.matrix.dhanashri.corejava;
+
+public class InstanceVariablesNew {
+
+    public static void main(String[] args) {
+        InstanceVariables obj= new InstanceVariables();
+        System.out.println(obj.age);
+        System.out.println(obj.str);
+        System.out.println(obj.b);
+        System.out.println(obj.arr);
+        obj.m1();
+        obj.m2();
+
+    }
+}
---
+++ src/main/java/com/matrix/dhanashri/corejava/StaticVariablesNew.java
@@ -0,0 +1,8 @@
+package com.matrix.dhanashri.corejava;
+
+public class StaticVariablesNew {
+
+    public static void main(String[] args) {
+        System.out.println(StaticVariable.age);
+    }
+}
