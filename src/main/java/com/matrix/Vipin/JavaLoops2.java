5a14b37bfb8b05330e59c87ecd5889af125d3797
Vipin Yadav <vipin.david72@gmail.com>
2022-06-25 19:07:14 +0530

    My Java loop Table2

--- src/main/java/com/matrix/Vipin/JavaLoops2.java
+++ src/main/java/com/matrix/Vipin/JavaLoops2.java
@@ -7,8 +7,10 @@ public class JavaLoops2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int mul = 1;
         for (int i = 2; i<35; i++){
-            System.out.println(i*n);
+            mul = i*n;
+            System.out.println(mul);
         }
     }
 }
