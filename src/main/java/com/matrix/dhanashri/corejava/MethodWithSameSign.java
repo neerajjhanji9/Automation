d3bfa2c8dbe15ce8f88f539e265e6f12980f7966
Dhanashri Bhosale <dhanshree.bhosale16@gmail.com>
2022-06-25 22:57:22 +0530

    All Method Concept files is added.

---
+++ src/main/java/com/matrix/dhanashri/corejava/MethodWithSameSign.java
@@ -0,0 +1,11 @@
+package com.matrix.dhanashri.corejava;
+
+public class MethodWithSameSign {
+    public void m1(String str1){
+        System.out.println(str1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
