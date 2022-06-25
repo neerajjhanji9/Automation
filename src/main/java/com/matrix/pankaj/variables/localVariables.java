8fa458f23a3c43844808913ebcc4a55c5f491ad2
HP <pankajkhobragade17@gmail.com>
2022-06-25 14:26:37 +0530

    New java

---
+++ src/main/java/com/matrix/pankaj/variables/localVariables.java
@@ -0,0 +1,12 @@
+package com.matrix.pankaj.variables;
+
+public class localVariables {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+
+
+    }
+
+}
