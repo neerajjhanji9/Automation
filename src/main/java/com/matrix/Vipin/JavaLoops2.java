7a15ebcef2463f6ac5a498dc766e8821eb96c2ac
Vipin Yadav <vipin.david72@gmail.com>
2022-06-25 19:00:54 +0530

    My Java loop Table

---
+++ src/main/java/com/matrix/Vipin/JavaLoops2.java
@@ -0,0 +1,15 @@
+package com.matrix.Vipin;
+
+import java.util.*;
+
+public class JavaLoops2 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 2; i<35; i++){
+
+            System.out.println(i*n);
+        }
+    }
+}
