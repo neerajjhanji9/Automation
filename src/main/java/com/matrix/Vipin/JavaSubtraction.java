42d39b9a6437ff74892bfc7dbc4177cc2ca357a5
Vipin Yadav <vipin.david72@gmail.com>
2022-06-26 17:07:07 +0530

    My Java Addition

---
+++ src/main/java/com/matrix/Vipin/JavaSubtraction.java
@@ -0,0 +1,11 @@
+package com.matrix.Vipin;
+
+public class JavaSubtraction {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int c =30;
+        int sum = a +c;
+        System.out.println(sum);
+    }
+}
