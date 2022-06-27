a3de8cd7b8ca66e5843671f1f65cdd6f0417f049
Amita Mandal <amitasingam@gmail.com>
2022-06-27 20:24:25 +0530

    Addition Example Method with calling

--- src/main/java/com/matrix/amita/corejava/Methods/MethodWithParameters.java
+++ src/main/java/com/matrix/amita/corejava/Methods/MethodWithParameters.java
@@ -1,13 +1,20 @@
 package com.matrix.amita.corejava.Methods;
 
+import javax.imageio.stream.ImageInputStream;
+
 public class MethodWithParameters {
 
-    public void addition(int a, int b){
+    public void addition(int a, int b) {
         System.out.println(a+b);
     }
 
+    public void multiply(int a, int b) {
+        System.out.println(a*b);
+    }
+
     public static void main(String[] args) {
-        MethodWithParameters obj=new MethodWithParameters();
-        obj.addition(20,50);
+        MethodWithParameters obj = new MethodWithParameters();
+        obj.addition(20, 50);
+        obj.multiply(10, 15);
     }
 }
