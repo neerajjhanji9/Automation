b27f4dbdfe3159720e2bce19e77778f0c4fbea08
Amita Mandal <amitasingam@gmail.com>
2022-06-28 19:25:11 +0530

    Method with Local Variable Priority

--- src/main/java/com/matrix/amita/corejava/Methods/MethodWithLocalVariablePriority.java
+++ src/main/java/com/matrix/amita/corejava/Methods/MethodWithLocalVariablePriority.java
@@ -9,11 +9,18 @@ public class MethodWithLocalVariablePriority {
         System.out.println(this.a+b);
         System.out.println(a+this.b);
         System.out.println(this.a+this.b); //it is taking from instance variable
+        this.a=a;
+        this.b=b;
     }
 
+    public void getData(){
+        System.out.println(a);
+        System.out.println(b);
+    }
     public static void main(String[] args) {
         MethodWithLocalVariablePriority obj = new MethodWithLocalVariablePriority();
         obj.add(10, 12);
+        obj.getData();
     }
 
 }
