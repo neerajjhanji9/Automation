970dcf4e1d2601e683c385978ad723a44b83fcb6
Amita Mandal <amitasingam@gmail.com>
2022-06-28 15:44:44 +0530

    Instance Variable access from another class

---
+++ src/main/java/com/matrix/amita/corejava/variables/InstanceVariableAccess.java
@@ -0,0 +1,9 @@
+package com.matrix.amita.corejava.variables;
+
+public class InstanceVariableAccess {
+
+    public static void main(String[] args) {
+        InstanceVariable obj = new InstanceVariable();
+        System.out.println();
+    }
+}
