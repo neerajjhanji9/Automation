3fb41f54f7fe3bacdd7e009fe9f18acb1fcf3a5b
Rahulyelgetti <rahulyelgetti444@gmail.com>
2022-06-29 15:39:58 +0530

    Practiced Instance variable

---
+++ src/main/java/com/matrix/rahuly/InstanceVariableProgram.java
@@ -0,0 +1,43 @@
+package com.matrix.rahuly;
+
+import java.util.Arrays;
+
+public class InstanceVariableProgram {
+
+    int a = 20;
+    int b = 30;
+
+    public void getData() {
+
+        System.out.println(a+b);
+    }
+    public void getInfo() {
+        int e = 10;
+        int f = 6;
+        System.out.println(e-f);
+    }
+
+    public static void getData1() {
+        String s = "Rahul";
+        int [] r = {100, 200, 300};
+        char [] ch = {'R', 'V', 'W'};
+        boolean i = true;
+
+        System.out.println(s);
+        System.out.println(Arrays.toString(r));
+        System.out.println(ch);
+        System.out.println(i);
+
+    }
+    public static void main(String[] args) {
+        int c = 5;
+        int d = 6;
+
+        InstanceVariableProgram obj = new InstanceVariableProgram();
+
+        System.out.println(obj.a + obj.b); //obj.getData(); - Another way for output.
+        obj.getInfo();
+        getData1();
+        System.out.println(c-d);
+    }
+}
