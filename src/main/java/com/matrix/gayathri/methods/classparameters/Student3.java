d9b5f77bbd8ccd53ff5a2a1744b38e8ec65ec709
Gayathri-Arangi <gayathri.arangi@gmail.com>
2022-06-30 10:18:07 +0530

    Class Parameters practice

---
+++ src/main/java/com/matrix/gayathri/methods/classparameters/Student3.java
@@ -0,0 +1,9 @@
+package com.matrix.gayathri.methods.classparameters;
+
+public class Student3 {
+    char section  ='C';
+    String name = "Jenny";
+    int roll_no= 303;
+    int maths = 39;
+    int science = 30;
+}
