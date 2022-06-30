3c16b10687a59e9f6f63cfac59102688dd33410e
Gayathri-Arangi <gayathri.arangi@gmail.com>
2022-06-30 10:23:32 +0530

    Class Parameters practice2

---
+++ src/main/java/com/matrix/gayathri/methods/Neighbour2.java
@@ -0,0 +1,10 @@
+package com.matrix.gayathri.methods;
+
+public class Neighbour2 {
+    char wing = 'B';
+    String name = "Jadhav";
+    int d_no= 302;
+    String occupation = "Teacher";
+    int age = 45;
+
+}
