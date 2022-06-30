c1c78a5d92f2d76a1097853a0ab1d693bbbb10a8
juhibb <juhibhandekar11@gmail.com>
2022-06-30 12:08:42 +0530

    Exercise no. 2 multiplication of two numbers using integer

---
+++ src/main/java/com/matrix/juhi/Exercise2.java
@@ -0,0 +1,14 @@
+package com.matrix.juhi;
+
+public class Exercise2 {
+
+     public void main(String[] args){
+
+         int a= 70;
+         int b= 10;
+         int c;
+         c = a*b;
+         System.out.println(c);
+     }
+    }
+
