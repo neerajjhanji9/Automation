7a09f724d336cdf392bb363d50f39c4e392b4a7a
Dhanashri Bhosale <dhanshree.bhosale16@gmail.com>
2022-07-01 14:05:05 +0530

    Constructor files are added

---
+++ src/main/java/com/matrix/dhanashri/corejava/ConstructorTest.java
@@ -0,0 +1,12 @@
+package com.matrix.dhanashri.corejava;
+
+public class ConstructorTest {
+
+    public static void main(String[] args) {
+        StudentClass st1= new StudentClass(10, "Dhanashri");
+        StudentClass st2= new StudentClass();
+
+
+    }
+
+}
