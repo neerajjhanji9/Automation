2c46e9b5c363310a74082a53606fdf2bcb8def5b
motilalmutkiri58 <motilal.mutkiri58@gmail.com>
2022-07-03 16:55:12 +0530

    This is Method Types program

---
+++ src/main/java/com/matrix/motilal/methods/PredefinedMethod.java
@@ -0,0 +1,7 @@
+package com.matrix.motilal.methods;
+
+public class PredefinedMethod {
+    public static void main(String[] args) {
+        System.out.println("println method from PrintStream class");
+    }
+}
