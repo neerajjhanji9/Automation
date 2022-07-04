01b703d7c8fc7d33f0efc9e5bca843f6b8667e45
kishormane2023 <kishormane2023@gmail.com>
2022-07-04 21:33:57 +0530

    Testing for Branch Push

--- src/main/java/com/matrix/kishor/corejava/instanceblock/InstanceBlock1.java
+++ src/main/java/com/matrix/kishor/corejava/instanceblock/InstanceBlock1.java
@@ -2,9 +2,10 @@ package com.matrix.kishor.corejava.instanceblock;
 
 public class InstanceBlock1 {
     int a;
-
+    int b;
     {
         a=30;
+        b=20;
         System.out.println("In instance block");
     }
     public InstanceBlock1(){
