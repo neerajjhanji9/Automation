3391d00b1a163be38ee5d2d0dcf845a42aea0435
kishormane2023 <kishormane2023@gmail.com>
2022-07-04 22:43:46 +0530

    String creation

--- src/main/java/com/matrix/kishor/corejava/stringconcept/TestString.java
+++ src/main/java/com/matrix/kishor/corejava/stringconcept/TestString.java
@@ -5,10 +5,13 @@ public class TestString {
      public static void main(String[] args) {
         String str = "Kishor"; //using string literal
          System.out.println(str);
+        String str1 = "Kishor";
+         System.out.println(str==str1); //refer same reference address
 
-         String str1 = new String("Kishor Mane");//String Object
-         System.out.println(str1);
-
-
+         String str2 = new String("Kishor Mane");//String Object
+         System.out.println(str2);
+         String str3 = new String("Kishor Mane");//String Object
+         System.out.println(str3);
+         System.out.println(str2==str3); //refer the different reference address
     }
 }
