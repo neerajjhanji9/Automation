cdfe1224d0a0a97d332134f9c7256420c938dc6f
Vipin Yadav <vipin.david72@gmail.com>
2022-07-04 16:47:47 +0530

    My Java Method Chaining2

---
+++ src/main/java/com/matrix/vipin/methods/MethodWithSameSignature.java
@@ -0,0 +1,15 @@
+package com.matrix.vipin.methods;
+
+public class MethodWithSameSignature {
+
+    public void m1(String str) {
+
+    }
+
+    public void m2(String str){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
