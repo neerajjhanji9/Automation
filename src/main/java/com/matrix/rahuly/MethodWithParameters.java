1e325b66d07f1a233460b1ff576567070d39028d
Rahulyelgetti <rahulyelgetti444@gmail.com>
2022-07-04 20:50:00 +0530

    Practiced Method with Parameters

---
+++ src/main/java/com/matrix/rahuly/MethodWithParameters.java
@@ -0,0 +1,33 @@
+package com.matrix.rahuly;
+
+public class MethodWithParameters {
+
+    public void getData(int a, int b){
+        System.out.println(a/b);
+
+    }
+
+    public void getInfo(boolean b, char ch){
+        System.out.println(b);
+        System.out.println(ch);
+    }
+
+    public void getName(String nm, int a, int b){
+        System.out.println(nm);
+        System.out.println(a*b);
+
+    }
+
+    public static void getData1(int e, int d, int f){
+        System.out.println(e+d*f);
+    }
+
+    public static void main(String[] args){
+        MethodWithParameters obj = new MethodWithParameters();
+        obj.getData(10, 5);
+        obj.getInfo(true, 'R');
+        obj.getName("Rahul",10,8);
+        getData1(5,10,20);
+
+    }
+}
