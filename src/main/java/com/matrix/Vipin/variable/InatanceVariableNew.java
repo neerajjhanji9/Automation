1776949f8c8cc8de5d2ff888ded5da2f9fecfe59
Vipin Yadav <vipin.david72@gmail.com>
2022-07-04 13:47:51 +0530

    My Java Variable object creation

---
+++ src/main/java/com/matrix/Vipin/variable/InatanceVariableNew.java
@@ -0,0 +1,10 @@
+package com.matrix.Vipin.variable;
+
+public class InatanceVariableNew {
+
+    public static void main(String[] args) {
+        InstanceVariable obj = new InstanceVariable();
+        System.out.println(obj.age);
+        System.out.println(obj.name);// ctrl+ click to know location of access instance variable
+    }
+}
