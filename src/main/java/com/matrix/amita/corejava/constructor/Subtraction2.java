ada7481bbd7f95320689007faf61c0d3f296afbc
Amita Mandal <amitasingam@gmail.com>
2022-07-04 19:40:52 +0530

    Subtraction in Constructor

---
+++ src/main/java/com/matrix/amita/corejava/constructor/Subtraction2.java
@@ -0,0 +1,16 @@
+package com.matrix.amita.corejava.constructor;
+
+public class Subtraction2 {
+
+    public Subtraction2(){
+        int a=100;
+        int b=90;
+        int c=a-b;
+        System.out.println("Subtraction of a and b is:" + c);
+    }
+
+
+    public static void main(String[] args) {
+        Subtraction2 obj=new Subtraction2();
+    }
+}
