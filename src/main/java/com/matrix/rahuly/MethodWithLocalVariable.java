33fc38babdd620c2020ea629934d4154dfa55dca
Rahulyelgetti <rahulyelgetti444@gmail.com>
2022-07-05 20:44:08 +0530

    Practiced method with local variable

---
+++ src/main/java/com/matrix/rahuly/MethodWithLocalVariable.java
@@ -0,0 +1,35 @@
+package com.matrix.rahuly;
+
+public class MethodWithLocalVariable {
+
+    int a;
+    int b;
+    String s;
+
+    public void getData(int a, int b, String s1){
+        this.a = a;
+        this.b = b;
+        this.s = s1;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(s1);
+        System.out.println(this.b / a);
+        System.out.println(a - this.b);
+    }
+
+    public void getData1(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println(a - this.b);
+        System.out.println(this.b * a);
+    }
+
+    public static void main(String[] args){
+        MethodWithLocalVariable m1 = new MethodWithLocalVariable();
+        m1.getData(10,20,"Rahul");
+        m1.getData1(15,10);
+
+    }
+
+
+}
