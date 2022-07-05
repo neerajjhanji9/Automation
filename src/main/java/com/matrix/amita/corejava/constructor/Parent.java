1e93ae711200a05d8971b8b9ba4212ccec24f7be
Amita Mandal <amitasingam@gmail.com>
2022-07-05 17:02:11 +0530

    Child extends Parents

---
+++ src/main/java/com/matrix/amita/corejava/constructor/Parent.java
@@ -0,0 +1,9 @@
+package com.matrix.amita.corejava.constructor;
+
+public class Parent {
+
+    Parent(){
+        // super(); it allow super constructor but it goes to object class
+        System.out.println("I am in Parent Constructor");
+    }
+}
