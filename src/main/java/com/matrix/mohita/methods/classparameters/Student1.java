9a462f3b1b2c10115a04a5dd751e85f9aae21963
HOME <ma>
2022-07-05 17:22:52 +0530

    ClassParameters practice

---
+++ src/main/java/com/matrix/mohita/methods/classparameters/Student1.java
@@ -0,0 +1,11 @@
+package com.matrix.mohita.methods.classparameters;
+
+public class Student1 {
+    char section = 'A';
+    String name = "Mahi";
+    int roll_no = 302;
+    int maths = 35;
+    int science = 50;
+
+
+}
