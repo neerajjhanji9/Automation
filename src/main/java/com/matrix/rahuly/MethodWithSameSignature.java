94401c8d8c3f9ea07208447b01a6fbcf920b91d4
Rahulyelgetti <rahulyelgetti444@gmail.com>
2022-07-05 23:48:17 +0530

    Practiced method with same signature

---
+++ src/main/java/com/matrix/rahuly/MethodWithSameSignature.java
@@ -0,0 +1,33 @@
+package com.matrix.rahuly;
+
+public class MethodWithSameSignature {
+
+    public void getData(String s1) {
+        System.out.println(s1);
+    }
+
+    public void getData(char ch) {
+        System.out.println(ch);
+    }
+
+    public void getInfo(int a, int b, boolean c) {
+        System.out.println(a == b);
+        System.out.println(c);
+    }
+
+    public void getClass(int a, int b, boolean c) {
+        System.out.println(a + b);
+        System.out.println(c);
+
+    }
+
+    public static void main(String[] args) {
+        MethodWithSameSignature m1 = new MethodWithSameSignature();
+        m1.getData("Rahul");
+        m1.getData('N');
+        m1.getInfo(10, 20, true);
+        m1.getClass(14, 5, false);
+
+    }
+
+}
