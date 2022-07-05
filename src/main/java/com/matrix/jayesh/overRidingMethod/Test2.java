d4bf208ca2b1d564fa3ef31419745c233e8aceea
jayeshpansare <jayesh.pansare.l@gmail.com>
2022-07-05 18:21:33 +0530

    overriding and overloading

---
+++ src/main/java/com/matrix/jayesh/overRidingMethod/Test2.java
@@ -0,0 +1,8 @@
+package com.matrix.jayesh.overRidingMethod;
+
+public class Test2 {
+    public boolean add(int i){
+        System.out.println("Return is "+i);
+        return false;
+    }
+}
---
+++ src/main/java/com/matrix/jayesh/overloading/ChildTest.java
@@ -0,0 +1,27 @@
+package com.matrix.jayesh.overloading;
+/**
+ * Adv -
+ * Method overloading increases the readability of the program.
+ *
+ * Note - Not possible on changing or multiple inheritance.
+ *
+ * Two ways
+ * - By changing number of arguments
+ * - By changing the data type
+ * **/
+public class ChildTest {
+    public static void main(String[] args) {
+        ParentTest obj = new ParentTest();
+        obj.subStract(); // By changing number of arguments
+        obj.subStract(10,20); // By changing number of arguments
+        obj.subStract(10,20,30); // By changing number of arguments
+
+
+        double testNo = obj.subStract(10.5, 20.5, 30.5); // By changing the data type
+
+        System.out.println(testNo); // call return type
+        System.out.println(obj.subStract(10.5, 20.5, 30.5)); // another way to call
+
+
+    }
+}
