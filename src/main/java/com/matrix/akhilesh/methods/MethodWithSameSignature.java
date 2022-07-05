8cc372c19420d5cd11d378c2e1ab85837655cc0b
Akhilesh Dandi <akhileshdandi@gmail.com>
2022-07-05 19:32:17 +0530

    Practice on New More Concepts of Methods & Practice on Palindrome Program

---
+++ src/main/java/com/matrix/akhilesh/methods/MethodWithSameSignature.java
@@ -0,0 +1,16 @@
+package com.matrix.akhilesh.methods;
+
+public class MethodWithSameSignature {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void m1(String str){
+        System.out.println(str);
+    }
+
+    /*public void m1(String str2){
+
+    }*/
+}
