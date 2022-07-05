f975b39a23c7ebeab4cd8d949cb00f8b31a5785e
ShikhaUdaiPratapSingh <shikha.prasang@gmail>
2022-07-05 00:49:26 -0400

    20220708 - Added Variables Types

---
+++ src/main/java/com/matrix/shikha/corejava/variables/InstanceVariablesNew.java
@@ -0,0 +1,11 @@
+package com.matrix.shikha.corejava.variables;
+
+public class InstanceVariablesNew {
+
+    public static void main(String[] args) {
+
+        InstanceVariable obj = new InstanceVariable();
+        System.out.println(obj.age);
+        System.out.println(obj.str);
+    }
+}
