bef9c0a832ad205c3bbe79be0d556b3d6d7f6fe1
Dhanashri Bhosale <dhanshree.bhosale16@gmail.com>
2022-07-07 11:36:58 +0530

    All Practice programs added

---
+++ src/main/java/com/matrix/dhanshri/corejava/simpleprograms/TestPractice1.java
@@ -0,0 +1,15 @@
+package com.matrix.dhanshri.corejava.simpleprograms;
+
+public class TestPractice1 {
+    void TestPractice1(){
+        System.out.println("Amsterdam");
+    }
+
+    TestPractice1(){
+        System.out.println("Antartica");
+    }
+
+    public static void main(String[] args) {
+       TestPractice1 tc= new TestPractice1(); ///only constructor can run: Antartica is o/p
+    }
+}
---
+++ src/main/java/com/matrix/dhanshri/corejava/simpleprograms/TestPractice4.java
@@ -0,0 +1,8 @@
+package com.matrix.dhanshri.corejava.simpleprograms;
+
+public class TestPractice4 {
+    public static void main(String[] args) {
+        String s= new String("Hello World");     //there are 2 string objects : 1 is in heap memory & other in SCP
+
+    }
+}
