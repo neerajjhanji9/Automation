3f7d41bbbfd18987c5da07462410dd5fc85574e6
juhibb <juhibhandekar11@gmail.com>
2022-07-08 15:18:39 +0530

    Constructor Chaining 2

--- src/main/java/com/matrix/juhi/constructor/ConstructorChaining.java
+++ src/main/java/com/matrix/juhi/constructor/ConstructorChaining.java
@@ -3,18 +3,25 @@ package com.matrix.juhi.constructor;
 public class ConstructorChaining {
 
     ConstructorChaining(int a, int b) {
-        this();
+
         System.out.println("Happy Monsoon");
     }
     ConstructorChaining(){
+        this("juhi");
         System.out.println("Constructor Chaining");
+
+    }
+
+    ConstructorChaining(String name){
+        System.out.println("Happy Summer");
     }
     public void add(){
 
     }
 
     public static void main(String[] args) {
-        new ConstructorChaining (20,30);
+        new ConstructorChaining  (20,30);
+        new ConstructorChaining();
     }
 
 
