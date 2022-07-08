018e056aad0100f7a9aec0fd151c48fd488c132c
jayeshpansare <jayesh.pansare.l@gmail.com>
2022-07-08 10:56:52 +0530

    run time polymorphism

--- src/main/java/com/matrix/jayesh/finalKeywords/FinalMethodA.java
+++ src/main/java/com/matrix/jayesh/finalKeywords/FinalMethodA.java
@@ -5,4 +5,6 @@ public class FinalMethodA {
         System.out.println("Test method");
     }
 
+
+
 }
---
+++ src/main/java/com/matrix/jayesh/runtimePolymorphism/ClassM.java
@@ -0,0 +1,13 @@
+package com.matrix.jayesh.runtimePolymorphism;
+
+public class ClassM extends ClassX{
+
+    public static void main(String[] args) {
+       // ClassX obj = new ClassM(); // here parent class and child constructor  //upcasting
+       // obj.set();
+
+        ClassN obj = new ClassX(); // Test class x  //upcasting
+        obj.set();
+    }
+
+}
