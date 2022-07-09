c2a0428bb086a7e2e8561e003ad9cc2c414e2e13
Ajaykumarbidgar <ajaykumarbidgar93@gmail.com>
2022-07-09 21:40:39 +0530

    Added new java file

---
+++ src/main/java/com/matrix/ajaykumar/corejava/HelloGitBranch.java
@@ -0,0 +1,8 @@
+package com.matrix.ajaykumar.corejava;
+
+public class HelloGitBranch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello welcome to Git branching concept");
+    }
+}
