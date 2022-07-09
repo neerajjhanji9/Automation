d15e7058fd137bfa5e4f8dbf4b4cac399b2fb8dc
jayeshpansare <jayesh.pansare.l@gmail.com>
2022-07-09 20:39:10 +0530

    Encapsulation

--- src/main/java/com/matrix/jayesh/InterfaceTest/TestClass.java
+++ src/main/java/com/matrix/jayesh/InterfaceTest/TestClass.java
@@ -3,7 +3,7 @@ package com.matrix.jayesh.InterfaceTest;
 public class TestClass implements ClassA, Showable {
     /**
      * Multiple inheritance in Java by interface
-     *
+     * Note: A class cannot be private or protected except nested class.
      * **/
     public void display(){
         System.out.println("Test ");
