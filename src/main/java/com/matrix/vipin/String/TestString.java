da7f3c191a69e5a40b4d3bd072ec5740357d3076
Vipin Yadav <vipin.david72@gmail.com>
2022-07-10 14:17:30 +0530

    My Java String Method

---
+++ src/main/java/com/matrix/vipin/String/TestString.java
@@ -0,0 +1,19 @@
+package com.matrix.vipin.String;
+
+public class TestString {
+
+
+    public static void main(String[] args) {
+
+     //  1.  By using object way
+        String Str1 = new String("Automation");
+        String Str2 = new String("Automation");
+        System.out.println(Str1 == Str2);
+      //  2. By using literal way
+        String Str3 = "Vipin";
+        String Str4 = "Vipin";
+        System.out.println(Str3 == Str4);
+
+
+    }
+}
