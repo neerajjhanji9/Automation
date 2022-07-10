5bec22961ca3baebfbbdeb635abd6b6693f1e633
Ajaykumarbidgar <ajaykumarbidgar93@gmail.com>
2022-07-10 17:48:03 +0530

    LocalVariables

--- src/main/java/com/matrix/ajaykumar/corejava/variables/LocalVariables.java
+++ src/main/java/com/matrix/ajaykumar/corejava/variables/LocalVariables.java
@@ -4,7 +4,10 @@ public class LocalVariables {
 
     public static void main(String[] args) {
         //code,logic
+        //Local Variable
         int a=10;
+        int b=20;
         System.out.println(a);
+        System.out.println(b);
     }
 }
