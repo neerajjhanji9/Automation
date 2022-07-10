f30a2a30762f8ccb21cc0a6851c7b13e537288f5
Ajaykumarbidgar <ajaykumarbidgar93@gmail.com>
2022-07-10 18:19:56 +0530

    InstanceVariable

---
+++ src/main/java/com/matrix/ajaykumar/corejava/variables/InstanceVariable.java
@@ -0,0 +1,29 @@
+package com.matrix.ajaykumar.corejava.variables;
+
+public class InstanceVariable {
+    //Instance Variable
+    int age =10;
+    String str="Ajaykumar";
+    int[] arr={10,20};
+    boolean b =true;
+    public void m1(){
+        int age=30;
+        System.out.println(age);
+
+    }
+    //Instance method
+    public void m2(){
+        //Instance area
+    }
+    // Static method
+    public static  void m3(){
+        //Static Area
+    }
+      void InstanceVariables (){
+        int age=30;
+          System.out.println(age);
+
+    }
+
+
+}
