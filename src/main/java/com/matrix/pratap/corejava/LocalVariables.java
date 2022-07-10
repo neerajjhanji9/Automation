7d03b6c49fe69a79d59b92add2d31a37df8f854e
Pratap-11 <waghmodepratap@gmail.com>
2022-07-10 10:50:16 +0530

    New Git Branch

---
+++ src/main/java/com/matrix/pratap/corejava/LocalVariables.java
@@ -0,0 +1,9 @@
+package com.matrix.pratap.corejava.variables;
+
+public class LocalVariables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.println(a + b);
+    }
+}
---
+++ src/main/java/com/matrix/pratap/corejava/NewGitBranch.java
@@ -0,0 +1,8 @@
+package com.matrix.pratap.corejava;
+
+public class NewGitBranch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to New Git Branch");
+    }
+}
