e5dacbae78567f6d547a32d3d863f027ad657fcc
Gayathri-Arangi <gayathri.arangi@gmail.com>
2022-07-11 17:18:34 +0530

    string immutable

--- src/main/java/com/matrix/gayathri/stringconcept/TestStringImmutability.java
+++ src/main/java/com/matrix/gayathri/stringconcept/TestStringImmutability.java
@@ -5,12 +5,12 @@ public class TestStringImmutability {
     public static void main(String[] args) {
         // concat() method
         String st = "Gayathri";
-        st= st.concat(" Arangi");  // String Immutable = need to give reference explicitly
+        st= st.concat(" Arangi");  // String Immutable (Cannot modify) : need to give reference explicitly
         System.out.println(st);
 
         //StringBuffer() method
         StringBuffer s = new StringBuffer("Gayathri");
-        s.append(" Anil");               // String mutable
+        s.append(" Anil");               // String Buffer/ String builder is mutable
         System.out.println(s);
     }
 }
