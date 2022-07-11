34a585ebad2fcd1b8a07f6dca91bfdc13802cf51
HOME <ma>
2022-07-11 17:46:12 +0530

    Added Constructor package

--- src/main/java/com/matrix/mohita/constructors/Child.java
+++ src/main/java/com/matrix/mohita/constructors/Child.java
@@ -1,4 +1,13 @@
 package com.matrix.mohita.constructors;
 
-public class Child {
+public class Child extends Parent{
+
+    Child(){
+    // super() : compiler will add super() keyword as first statement by default
+        System.out.println("Im in Child constructor");
+    }
+
+    public static void main(String[] args) {
+        Child obj = new Child();
+    }
 }
