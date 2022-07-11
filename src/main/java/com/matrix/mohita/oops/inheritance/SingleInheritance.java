de19a26bb6e3e9643fc70d5fd724352710c3ba69
HOME <ma>
2022-07-11 20:07:04 +0530

    Oops-Inheritance Concept added

---
+++ src/main/java/com/matrix/mohita/oops/inheritance/SingleInheritance.java
@@ -0,0 +1,25 @@
+package com.matrix.mohita.oops.inheritance;
+
+/** Single Inheritance : one to one mapping
+ *          Animal
+ *             |
+ *            Dog
+ */
+ class Animal {
+     void eat (){
+         System.out.println("I am an animal and i eat meat");
+     }
+}
+ class Dog extends Animal{
+     void bark(){
+         System.out.println("Dog: barks");
+     }
+ }
+
+public class SingleInheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog ();
+        d.eat();
+        d.bark();
+    }
+}
