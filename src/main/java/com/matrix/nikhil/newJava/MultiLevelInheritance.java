06f6aee516a3d4fe9bad84806efd201df461eb8d
nikhilalle <allenikhilalle@gmail.com>
2022-07-12 23:04:17 +0530

    Method Programs

---
+++ src/main/java/com/matrix/nikhil/newJava/MultiLevelInheritance.java
@@ -0,0 +1,26 @@
+package com.matrix.nikhil.newJava;
+
+   class Vehicle {
+        Vehicle() {
+            System.out.println("I am In Vehicle Class");
+        }
+    }
+    class FourWheeler extends Vehicle{
+        FourWheeler(){
+            System.out.println("I am In Four-Wheeler Class");
+        }
+    }
+    class TwoWheeler extends FourWheeler{
+        TwoWheeler(){
+            System.out.println("I am In Two-rWheeler Class");
+        }
+       // public class MultiLevel{
+
+
+    public static void main(String[] args) {
+
+        TwoWheeler obj1 = new TwoWheeler();
+
+
+    }
+}
