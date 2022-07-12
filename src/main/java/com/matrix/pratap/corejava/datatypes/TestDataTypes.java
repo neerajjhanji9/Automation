95de23b0010a71a886cc86b1669b2750809b8315
Pratap-11 <waghmodepratap@gmail.com>
2022-07-12 12:36:54 +0530

    Test Data Types

---
+++ src/main/java/com/matrix/pratap/corejava/datatypes/TestDataTypes.java
@@ -0,0 +1,44 @@
+package com.matrix.pratap.corejava.datatypes;
+
+public class TestDataTypes {
+
+    static int rollNumber;
+
+    static char chr;
+
+    static byte by;
+
+    static boolean b;
+
+    static float fl;
+
+    static double dl;
+
+    static String st;
+
+    public static void main(String[] args) {
+
+        int age = 10;
+        boolean a = true;
+        char ch = 'S';
+        float f = 10.5f;
+        double d = 10.10;
+        String str = "Pratapsinha";
+
+        System.out.println(age);
+        System.out.println(a);
+        System.out.println(ch);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(str);
+        System.out.println("--------------------");
+        System.out.println(rollNumber);
+        System.out.println(chr);
+        System.out.println(by);
+        System.out.println(b);
+        System.out.println(fl);
+        System.out.println(dl);
+        System.out.println(st);
+    }
+
+}
