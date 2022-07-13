cccca142b82194f3a92f521fcd92affa06540ec5
vinayasankpal <vinayasankpal@gmail.com>
2022-07-13 16:40:41 +1000

    Added new java file

---
+++ src/main/java/com/matrix/vinaya/HelloNewGitBranch.java
@@ -0,0 +1,8 @@
+package com.matrix.vinaya;
+
+public class HelloNewGitBranch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to Git Branching Concept!");
+    }
+}
