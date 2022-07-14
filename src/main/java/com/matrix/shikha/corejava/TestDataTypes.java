b370e4ddd31cda6aa28da3968621ec0f2d386be4
ShikhaUdaiPratapSingh <shikha.prasang@gmail>
2022-07-13 23:58:58 -0400

    20220713 - Added Test Datatypes

--- src/main/java/com/matrix/shikha/corejava/TestDataTypes.java
+++ src/main/java/com/matrix/shikha/corejava/TestDataTypes.java
@@ -1,8 +1,7 @@
 package com.matrix.shikha.corejava;
 
 public class TestDataTypes {
-<<<<<<< HEAD
-=======
+
     static int rollnumber;
     static boolean b;
     static char chr;
@@ -10,16 +9,12 @@ public class TestDataTypes {
     static double dl;
     static String st;
 
->>>>>>> 18aac14dd5f0d30881a42a185a434eac17bb03b5
+
     public static void main(String[] args) {
         int age = 10;
         boolean a = true;
         char ch = 'S';
-<<<<<<< HEAD
-        float f = 10.5f;
-=======
         float f = 10.523f;
->>>>>>> 18aac14dd5f0d30881a42a185a434eac17bb03b5
         double d = 10.15;
 
         System.out.println(age);
@@ -28,18 +23,8 @@ public class TestDataTypes {
         System.out.println(f);
         System.out.println(d);
         System.out.println("\t"+ "My age is " + age +" " + a);
-<<<<<<< HEAD
+
 
     }
 }
-=======
-        System.out.println("----------------");
-        System.out.println(rollnumber);
-        System.out.println(b);
-        System.out.println(chr);
-        System.out.println(fl);
-        System.out.println(dl);
-        System.out.println(st);
-    }
-}
->>>>>>> 18aac14dd5f0d30881a42a185a434eac17bb03b5
+
