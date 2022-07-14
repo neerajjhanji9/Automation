5e487a05d395b72b3c5b376fda461d64b504c26a
neerajjhanji9 <neerajjhanji6@gmail.com>
2022-07-14 22:17:58 +0530

    Addition and Substraction

---
+++ src/main/java/com/matrix/neeraj/datatypes/AdditionOfFloat.java
@@ -0,0 +1,14 @@
+package com.matrix.neeraj.datatypes;
+
+public class AdditionOfFloat {
+    public static void main(String[] args) {
+
+        float fl = 10.1f;
+        float fl1 = 10.2f;
+        float fl2 = 10.3f;
+        float fl3 = 10.4f;
+        float fl4 = 10.5f;
+
+        System.out.println(fl + fl1 + fl2 + fl3 +fl4);
+    }
+}
