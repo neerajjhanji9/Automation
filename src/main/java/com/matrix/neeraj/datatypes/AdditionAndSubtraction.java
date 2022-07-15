5e9938c4c74e1664f520c5ba90106d10525902df
neerajjhanji9 <neerajjhanji6@gmail.com>
2022-07-15 14:11:51 +0530

    AdditionAndSubtraction

---
+++ src/main/java/com/matrix/neeraj/datatypes/AdditionAndSubtraction.java
@@ -0,0 +1,13 @@
+package com.matrix.neeraj.datatypes;
+
+public class AdditionAndSubtraction {
+
+    public static void main(String[] args) {
+        int ad = 30;
+        int bd = 10;
+        int cd = 10;
+
+        System.out.println(ad+bd+cd);
+        System.out.println(ad-bd-cd);
+    }
+}
